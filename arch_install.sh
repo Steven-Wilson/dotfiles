407714b4e9b61999e4be754ac8a95b83ae312cf1
Steven Wilson <stevenwilson500@gmail.com>
2015-09-15 08:21:26 -0400

    Create arch_install.sh

---
+++ arch_install.sh
@@ -0,0 +1,15 @@
+cd ~
+
+sudo pacman -Syu
+sudo pacman -S vim git mercurial subversion tree sdl2{,_image,_mixer,_ttf,       ~~~~~~~~~~~>_net,_gfx} terminator
+git clone github.com/Steven-Wilson/dotfiles
+git clone https://github.com/Steven-Wilson/dotfiles
+cp dotfiles/.profile ~/.profile
+cp dotfiles/.vimrc ~/.vimrc
+cp dotfiles/.gvimrc ~/.gvimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.      ~~~~~~~~~~~>vim
+git config --global user.name "Steven Wilson"
+git config --global user.email stevenwilson500@gmail.com
+git config --global core.editor vim
+wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.5.1.linux-amd64.tar.gz
