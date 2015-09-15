e50ded261ba51105f3682d0929bc37af8e0cce97
Steven Wilson <stevenwilson500@gmail.com>
2015-09-15 08:22:14 -0400

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -1,13 +1,13 @@
 cd ~
 
 sudo pacman -Syu
-sudo pacman -S vim git mercurial subversion tree sdl2{,_image,_mixer,_ttf,       ~~~~~~~~~~~>_net,_gfx} terminator
+sudo pacman -S gvim git mercurial subversion tree sdl2{,_image,_mixer,_ttf,_net,_gfx} terminator
 git clone github.com/Steven-Wilson/dotfiles
 git clone https://github.com/Steven-Wilson/dotfiles
 cp dotfiles/.profile ~/.profile
 cp dotfiles/.vimrc ~/.vimrc
 cp dotfiles/.gvimrc ~/.gvimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.      ~~~~~~~~~~~>vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git config --global user.name "Steven Wilson"
 git config --global user.email stevenwilson500@gmail.com
 git config --global core.editor vim
